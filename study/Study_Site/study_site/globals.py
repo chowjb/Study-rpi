e7469360a92f4fd456e52cf6b75c45fe1f5d4570
Jeff Chow <jeffbchow@gmail.com>
2021-12-27 09:46:23 -0800

    Delete study directory

--- study/Study_Site/study_site/globals.py
+++
@@ -1,2 +0,0 @@
-g_transport = None
-g_gcode_file = '/home/pi/bots/jandi/in_out/gcoded.txt'
--- study/venv/lib/gcode/ScoreStore.py
+++
@@ -1,8 +0,0 @@
-class ScoreStore:
-    """ Handles all scores in the dictionary """
-    def __init__(self):
-        self.scoreDict = {'Dual Infinity': 'DualInfinity.txt',
-                         'Graceful Duet': 'GracefulDuet.txt'}
-        
-    def getFilename(self, scoreTitle):
-        return self.scoreDict.get(scoreTitle, None)
