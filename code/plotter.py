0a88a7de7ee59c4c011d283983b7cef5c2e81cdb
chowjb <jeffbchow@gmail.com>
2022-04-01 18:12:58 -0700

    Start of Etch-A-Sketch plotter

--- code/plotter.py
+++ code/plotter.py
@@ -1,7 +1,5 @@
-#import time
-#import importlib
-#from in_out import grbl as transport
 import utilities.constants as constants
+#import importlib
 #importlib.reload(grbl)
 
 class Plotter:
@@ -27,16 +25,21 @@ class Plotter:
         self.transport.set_speed_accel('Y', 2000, 2000)
         self.transport.send_gcode(f'$100={steps_per_mm}')
         self.transport.send_gcode(f'$101={steps_per_mm}')
-        self.transport.send_gcode('$23=3')   # reverse direction for X/Y
-        self.transport.send_gcode('$27=10')   # short pull-off of home
+        self.transport.send_gcode('$23=1')  # reverse direction for X for home
+        self.transport.send_gcode('$3=2')   # reverse direction of stepper for Y
+        self.transport.send_gcode('$27=10') # short pull-off of home
         self.transport.home()
+        self.transport.send_gcode('G91 Y-350')
+        self.transport.send_gcode('G10 L20 P1 X0 Y0 Z0')
+        # save this as the permanent home position (it in machine coordinates)
+        self.transport.send_gcode('G28.1')
+        # set for absolute position
+        self.transport.send_gcode('G90')
 
-    # def plot(self, x, y, speed):
-    #     self.grbl.set_speed(speed)
-    #     self.grbl.move_to(x, y)
-    #     self.grbl.wait_for_idle()
-    #     self.grbl.set_speed(constants.DEFAULT_SPEED)
-    #     self.grbl.set_feed(constants.DEFAULT_FEED)
+    def plot(self, x, y, speed=2000, accel=2000):
+        self.transport.set_speed_accel('X', speed, accel)
+        self.transport.set_speed_accel('Y', speed, accel)
+        self.transport.move_to(x, y)
 
     # def plot_line(self, x, y, speed):
     #     self.grbl.set_speed(speed)
